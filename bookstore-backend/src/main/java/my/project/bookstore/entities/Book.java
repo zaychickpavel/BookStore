83c23ab84f2416a962a277613a62c4d9f73fb430
zaychickpavel <zaychick.pav@yandex.ru>
2017-01-23 22:18:23 +0300

    update database structure

--- bookstore-backend/src/main/java/my/project/bookstore/entities/Book.java
+++ bookstore-backend/src/main/java/my/project/bookstore/entities/Book.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Arrays;
 
 /**
  * Created by zaychick-pavel on 1/17/17.
@@ -21,6 +22,24 @@ public class Book {
 	@Column(name = "name")
 	private String name;
 
+	@Column(name = "original_name")
+	private String originalName;
+
+	@Column(name = "publishing_year")
+	private Integer publishingYear;
+
+	@Column(name = "number_of_pages")
+	private Integer numberOfPages;
+
+	@Column(name = "isbn")
+	private String isbn;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "image")
+	private byte[] image;
+
 	public Integer getId() {
 		return id;
 	}
@@ -35,5 +54,67 @@ public class Book {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getOriginalName() {
+		return originalName;
+	}
+
+	public void setOriginalName(String originalName) {
+		this.originalName = originalName;
+	}
+
+	public Integer getPublishingYear() {
+		return publishingYear;
+	}
+
+	public void setPublishingYear(Integer publishingYear) {
+		this.publishingYear = publishingYear;
+	}
+
+	public Integer getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	public void setNumberOfPages(Integer numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "Book{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", originalName='" + originalName + '\'' +
+				", publishingYear=" + publishingYear +
+				", numberOfPages=" + numberOfPages +
+				", isbn='" + isbn + '\'' +
+				", description='" + description + '\'' +
+				", image=" + Arrays.toString(image) +
+				'}';
 	}
 }
--- bookstore-backend/src/main/resources/sql/1_create_tables.sql
+++ bookstore-backend/src/main/resources/sql/1_create_tables.sql
@@ -1,6 +1,43 @@
--- BOOK
 CREATE TABLE BOOK (
-  id            INTEGER PRIMARY KEY,
-  name        TEXT NOT NULL
+	id              INTEGER PRIMARY KEY AUTOINCREMENT,
+	name            TEXT NOT NULL,
+	original_name   TEXT,
+	publishing_year INTEGER,
+	number_of_pages INTEGER,
+	isbn            TEXT,
+	description     TEXT,
+	image           BLOB,
+	publisher_id    INTEGER
 );
 
+CREATE TABLE AUTHOR (
+	id          INTEGER PRIMARY KEY AUTOINCREMENT,
+	name        TEXT NOT NULL,
+	description TEXT,
+	image       BLOB
+);
+
+CREATE TABLE GENRE (
+	id        INTEGER PRIMARY KEY AUTOINCREMENT,
+	name      TEXT,
+	parent_id INTEGER
+);
+
+CREATE TABLE PUBLISHER (
+	id          INTEGER PRIMARY KEY AUTOINCREMENT,
+	name        TEXT NOT NULL,
+	description TEXT,
+	image       BLOB
+);
+
+CREATE TABLE BOOK_GENRE (
+	id       INTEGER PRIMARY KEY AUTOINCREMENT,
+	book_id  INTEGER,
+	genre_id INTEGER
+);
+
+CREATE TABLE BOOK_AUTHOR (
+	id        INTEGER PRIMARY KEY AUTOINCREMENT,
+	book_id   INTEGER,
+	author_id INTEGER
+);
