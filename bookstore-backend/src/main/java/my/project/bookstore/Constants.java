e25745f1d839d018a98b9b85ad018a4ddecf8f1f
zaychickpavel <zaychick.pav@yandex.ru>
2017-01-24 22:21:21 +0300

    remove incorrect import

--- bookstore-backend/src/main/java/my/project/bookstore/Constants.java
+++ bookstore-backend/src/main/java/my/project/bookstore/Constants.java
@@ -1,10 +1,5 @@
 package my.project.bookstore;
 
-import my.project.bookstore.model.Book;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by zaychick-pavel on 1/12/17.
  */
