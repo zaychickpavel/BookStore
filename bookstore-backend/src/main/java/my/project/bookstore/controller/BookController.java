d2160b805acda075df48fa6daeb4bc087cf74aa7
zaychickpavel <zaychick.pav@yandex.ru>
2017-01-24 17:37:39 +0300

    add rest controller for book

--- bookstore-backend/src/main/java/my/project/bookstore/controller/BookController.java
+++ bookstore-backend/src/main/java/my/project/bookstore/controller/BookController.java
@@ -5,6 +5,7 @@ import my.project.bookstore.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,23 +25,37 @@ public class BookController {
 	BookDAO books;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Book> getBooks() {
-		List<Book> result = new ArrayList<Book>();
+	public List<Book> getAll() {
+		List<Book> result = new ArrayList<>();
 		books.findAll().forEach(result::add);
 		return result;
-
-//		return Constants.BOOK_LIST;
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Book getBook(@PathVariable("id") int id) {
+	@RequestMapping(method = RequestMethod.GET, value = "{id}")
+	public Book get(@PathVariable int id) {
 		return books.findOne(id);
-//
-//		for (Book book : Constants.BOOK_LIST) {
-//			if (book.getId() == id) {
-//				return book;
-//			}
-//		}
-//		return null;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Book add(@RequestBody Book book) {
+		return books.save(book);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+	public void delete(@PathVariable int id) {
+		books.delete(id);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "{id}")
+	public Book update(@PathVariable int id, @RequestBody Book book) {
+		Book update = books.findOne(id);
+		update.setName(book.getName());
+		update.setOriginalName(book.getOriginalName());
+		update.setIsbn(book.getIsbn());
+		update.setDescription(book.getDescription());
+		update.setNumberOfPages(book.getNumberOfPages());
+		update.setPublishingYear(book.getPublishingYear());
+		update.setImage(book.getImage());
+		return books.save(update);
 	}
 }
