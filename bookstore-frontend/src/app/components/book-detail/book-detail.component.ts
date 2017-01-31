542472a2980568f3fdee027f704e66de089d7e42
zaychickpavel <zaychick.pav@yandex.ru>
2017-01-31 18:00:02 +0300

    add dropdown to edit book mode

--- bookstore-frontend/src/app/components/book-detail/book-detail.component.ts
+++ bookstore-frontend/src/app/components/book-detail/book-detail.component.ts
@@ -12,8 +12,6 @@ import {PublisherService} from "../../services/publisher.service";
 import {Publisher} from "../../models/publisher";
 
 
-
-
 @Component({
 	moduleId: module.id,
 	selector: 'book-detail',
@@ -23,79 +21,105 @@ import {Publisher} from "../../models/publisher";
 
 export class BookDetailComponent implements OnInit {
 	@Input()
-	book: Book;
-	errorMessage: string;
-	editMode: boolean = false;
-	newBook: boolean = false;
+	book:Book;
+	errorMessage:string;
+	editMode:boolean = false;
+	newBook:boolean = false;
 
-	authors: Author[] = [];
-	genres: Genre[] = [];
-	publishers: Publisher[] = [];
+	authors:Author[] = [];
+	genres:Genre[] = [];
+	publishers:Publisher[] = [];
 
-	constructor(private bookService: BookService,
-				private authorService: AuthorService,
-				private genreService: GenreService,
-				private publisherService: PublisherService,
-				private route: ActivatedRoute,
-				private changeDetectorRef: ChangeDetectorRef,
-				private location: Location,
-				private router: Router) {
+	private authorItems:Array<any> = [];
+	private publisherItems:Array<any> = [];
+	private genreItems:Array<any> = [];
+
+
+	constructor(private bookService:BookService,
+				private authorService:AuthorService,
+				private genreService:GenreService,
+				private publisherService:PublisherService,
+				private route:ActivatedRoute,
+				private changeDetectorRef:ChangeDetectorRef,
+				private location:Location,
+				private router:Router) {
 	}
 
 
+	private getAuthors() {
+		this.authorService.getAll()
+			.subscribe(
+				authors => this.authors = authors,
+				error => this.errorMessage = <any>error,
+				() => this.authors.forEach((author:Author) => {
+					this.authorItems.push({
+						id: author.id,
+						text: author.name
+					});
+				}));
+	}
 
-	ngOnInit(): void {
-		let id: number;
+	private getGenres() {
+		this.genreService.getAll()
+			.subscribe(
+				genres => this.genres = genres,
+				error => this.errorMessage = <any>error,
+				() => this.genres.forEach((genre:Genre) => {
+					this.genreItems.push({
+						id: genre.id,
+						text: genre.name
+					});
+				}));
+	}
+
+	private getPublishers() {
+		this.publisherService.getAll()
+			.subscribe(
+				publishers => this.publishers = publishers,
+				error => this.errorMessage = <any>error,
+				() => this.publishers.forEach((publisher:Publisher) => {
+					this.publisherItems.push({
+						id: publisher.id,
+						text: publisher.name
+					});
+				}));
+	}
+
+	private getBook(id:number) {
+		this.bookService.get(id)
+			.subscribe(
+				book => this.book = book,
+				error => this.errorMessage = <any>error);
+	}
+
+	ngOnInit():void {
+		let id:number;
 		this.route.params.subscribe(params => {
 			id = +params['id'];
 		});
 
 		if (!isNaN(id)) {
-			this.bookService.get(id)
-				.subscribe(
-					book => this.book = book,
-					error => this.errorMessage = <any>error);
+			this.getBook(id);
 			this.newBook = false;
 		} else {
 			this.book = new Book();
 			this.editMode = true;
 			this.newBook = true;
-			this.authorService.getAll()
-				.subscribe(
-					authors => this.authors = authors,
-					error => this.errorMessage = <any>error);
-			this.genreService.getAll()
-				.subscribe(
-					genres => this.genres = genres,
-					error => this.errorMessage = <any>error);
-			this.publisherService.getAll()
-				.subscribe(
-					publishers => this.publishers = publishers,
-					error => this.errorMessage = <any>error);
+			this.getAuthors();
+			this.getGenres();
+			this.getPublishers();
 		}
 	}
 
-	change(fileList: FileList) {
+	change(fileList:FileList) {
 		new ImageUtils(this.changeDetectorRef, this.book).readImage(fileList);
 	}
 
 	edit() {
 		this.editMode = true;
-
-		this.authorService.getAll()
-			.subscribe(
-				authors => this.authors = authors,
-				error => this.errorMessage = <any>error);
-
-		this.genreService.getAll()
-			.subscribe(
-				genres => this.genres = genres,
-				error => this.errorMessage = <any>error);
-
-		this.publisherService.getAll()
-			.subscribe(
-				publishers => this.publishers = publishers,
-				error => this.errorMessage = <any>error);
+		this.getAuthors();
+		this.getGenres();
+		this.getPublishers();
 	}
 
 	save() {
@@ -123,10 +147,7 @@ export class BookDetailComponent implements OnInit {
 			this.newBook = false;
 			this.back();
 		} else {
-			this.bookService.get(this.book.id)
-				.subscribe(
-					book => this.book = book,
-					error => this.errorMessage = <any>error);
+			this.getBook(this.book.id);
 		}
 		this.editMode = false;
 	}
@@ -138,11 +159,11 @@ export class BookDetailComponent implements OnInit {
 				error => this.errorMessage = <any>error);
 	}
 
-	back(): void {
+	back():void {
 		this.location.back();
 	}
 
-	setSelectedAuthor(author: Author): boolean {
+	setSelectedAuthor(author:Author):boolean {
 		if (this.book.authors != null) {
 			for (var a of this.book.authors) {
 				if (a.id == author.id) {
@@ -153,7 +174,7 @@ export class BookDetailComponent implements OnInit {
 		return false;
 	}
 
-	setSelectedGenre(genre: Genre): boolean {
+	setSelectedGenre(genre:Genre):boolean {
 		if (this.book.genres != null) {
 			for (var a of this.book.genres) {
 				if (a.id == genre.id) {
@@ -162,6 +183,29 @@ export class BookDetailComponent implements OnInit {
 			}
 		}
 		return false;
+	}
+
+
+	public refreshPublisher(value:any):void {
+		this.setBookPublisher(value.id);
+	}
+
+	private setBookPublisher(id:number) {
+		for (let publisher of this.publishers) {
+			if (publisher.id == id) {
+				this.book.publisher = publisher;
+				break;
+			}
+		}
+	}
+
+	private getCurrentPublisher() {
+		if (this.book.publisher != null) {
+			//let current:Array<any> = [{id: this.book.publisher.id, name: this.book.publisher.name}];
+			return {id: this.book.publisher.id, name: this.book.publisher.name};
+		} else {
+			return null;
+		}
 	}
 
 
